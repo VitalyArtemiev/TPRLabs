d35cca7dd9580d7df955f5826ef4ca0210272f20
VitalyArtemiev <vitaly.artemiev@yandex.ru>
2019-03-11 11:06:19 +0300

    Finished amoeba algorithm

--- TPR2/amoeba.m
+++ TPR2/amoeba.m
@@ -21,30 +21,34 @@ asdd the same for cuts alosdg the two other axes. (Obviously the usdique calls s
 clear; close all; clc;
 %STEP 1
 n = 2
-m = 0.25
+m = 1
+
+beta = 2.8  #expand 2.8<beta<3
+gamma = 0.4 #contract 0.4<gamma<0.6
+
 eps = 0.1
 
-x0 = ones(1,n)
+x0 = zeros(1,n);
 
 x0n = zeros(n+1, n);
-x0n(1,:) = x0
+x0n(1,:) = x0;
 
 %STEP 2
 delta1 = (sqrt(n+1) - 1)/(n*sqrt(2)) * m
 delta2 = (sqrt(n+1) + n - 1)/(n*sqrt(2)) * m
 
 for i = 2:n+1
-    x0n(i, :) = x0n(1,:) .+ delta2;
-    x0n(i, i-1) = x0n(1,i-1) + delta1;
-  endfor
+  x0n(i, :) = x0n(1,:) .+ delta2;
+  x0n(i, i-1) = x0n(1,i-1) + delta1;
+endfor
   
   x0n
 
 finished = false;
 iteration = -1;
-RH = zeros(30,n)
-FH = zeros(30,1)
-TH = zeros(n+1, n, 30)
+RH = zeros(30,n);
+FH = zeros(30,1);
+TH = zeros(n+1, n, 30);
 
 while !finished
   iteration++
@@ -52,47 +56,110 @@ while !finished
   %STEP 3  
   fres = funcToOptimize(x0n);
   
-  [fmax, fmaxi] = max(fres);
-  disp("k = "); disp(fmaxi);
+  [fh, k] = max(fres);
+  [fl, k1] = min(fres);
+  fs = max(fres(fres<max(fres))) ;
+  k2 = find(fres == fs);
+  
+  disp("k = "); disp(k);
   sum(x0n, 1);
   
   %STEP 4
-  xk = x0n(fmaxi(1), :);
+  xk = x0n(k(1), :);
   xc = sum(x0n, 1) - xk; 
   xc /= n;
   
   %STEP 5
-  xnew = 2 * xc - xk
+  disp("reflection")
+  xnew = 2 * xc - xk;
   
   fnew = funcToOptimize(xnew);
   
   %STEP 6
-  if fnew < fmax
-    x0n(fmaxi(1), :) = xnew;
-  else   
-    %STEP 7
-    [fmin, fmini] = min(fres);
-    
-    minr = x0n(fmini(1),:)
-    
-    x0n = 0.5 * (x0n + minr)
-  endif
+  if fnew < fh %reflect successful
+    disp("reflection success")
+    x0n(k(1), :) = xnew;
+    xk = xnew;
+    fh = fnew;
+    nextStep = 7;
+  else
+    disp("reflection fail")
+    nextStep = 9;
+  end
+  
+  %STEP 7
+  if (nextStep == 7) && (fh < fl)
+    disp("expansion")
+    xnew = xc + beta * (xk - xc);
+    fnew = funcToOptimize(xnew);
+    nextStep = 8;
+  else
+    disp("no expansion")
+    nextStep = 9;
+  end
   
   %STEP 8
-  xc = sum(x0n, 1) / (n+1);
-  fc = funcToOptimize(xc)
-  RH(iteration + 1, :) = xc; 
-  FH(iteration + 1) = fc; 
-  
-  %STEP 9  
-  dif = abs(funcToOptimize(x0n) .- fc)
-  if sum(dif >= eps) == 0
-    finished = true;
+  if (nextStep == 8) && (fnew < fh) %expand successful
+    x0n(k(1), :) = xnew;   
+    nextStep = 12;
+    disp("expansion success")
+  else
+    disp("expansion fail")
+    nextStep = 9;
+  end
+ 
+  %STEP 9 
+  if (nextStep == 9) && (fs <= fnew) && (fnew < fh)
+    disp("contraction")
+    xnew = xc + gamma * (xnew - xc);
+    fnew = funcToOptimize(xnew);
+    nextStep = 10;
+  else
+    disp("no contraction")
+    nextStep = 11;  
   endif
+   
+  %STEP 10
+  if (nextStep == 10) && (fnew < fh) %funcToOptimize(x0n(k(1), :))) %contract successful
+    x0n(k(1), :) = xnew;  
+    nextStep = 12;
+    disp("contraction success")
+  else
+    disp("contraction fail")
+    nextStep = 11;
+  endif
+  
+  %STEP 11
+  if nextStep == 11
+    [fmin, fmini] = min(fres);
+    
+    minr = x0n(fmini(1),:);
+    
+    x0n = 0.5 * (x0n + minr);
+  endif
+  
+  %STEP 12
+  xc = sum(x0n, 1) / (n+1);
+  fc = funcToOptimize(xc);
+  disp("sigma calc")
+  funcToOptimize(x0n)
+  funcToOptimize(x0n) - fc
+  (funcToOptimize(x0n) - fc) .^ 2
+  sum((funcToOptimize(x0n) - fc) .^ 2)
+  sum((funcToOptimize(x0n) - fc) .^ 2) / (n + 1)
+  sigma = sqrt(sum((funcToOptimize(x0n) - fc) .^ 2) / (n + 1))
+          
+  %STEP 13
+  if sigma < eps
+    finished = true
+  endif
+
+  RH(iteration + 1, :) = xc; 
+  FH(iteration + 1) = sigma; 
  
   x0n
   TH(:,:,iteration+1) = x0n;
-  %input("waiting");
+  input("waiting");
 endwhile
 
 fres = funcToOptimize(x0n);
@@ -139,7 +206,7 @@ if n == 2
   
   % Contour plot
   figure;
-  % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+  % Plot J_vals as 15 contours spaced logarithmically between 0.01 && 100
   contour(theta0_vals, theta1_vals, J_vals, linspace(-3, 6, 30))
   xlabel('\theta_0'); ylabel('\theta_1');
   hold on;
